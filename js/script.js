2f9edf3d1d0cb62468270ac4903adb013ef4b2a9
SteCard <stefanocardillo14@gmail.com>
2023-06-20 00:25:38 +0200

    FIX - codice ottimizzato

--- js/script.js
+++ js/script.js
@@ -3,19 +3,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        mail: []
+        arrayMail: []
       }
     },
     mounted() {
-        this.functionRandom()
-    },
-    methods: {
-        functionRandom(){
-            for (let i=0; i<= 10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.mail.push(result.data.response);
-                });
-            };
-        }, 
-    },
-  }).mount('#app')
+      for (let i=1; i<=10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => 
+        {
+          this.arrayMail.push(result.data.response);
+        })
+      }
+    } 
+  }).mount('#app');
